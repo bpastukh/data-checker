6ac89669df4ebc0a84a6b7988e11dab796fa1284
Bohdan Pastukh <bg.pastukh@gmail.com>
2022-05-04 15:27:13 +0300

    daemonize, use logger instead of output

--- src/Command/CheckClickHouseCommand.php
+++ src/Command/CheckClickHouseCommand.php
@@ -6,6 +6,7 @@ namespace App\Command;
 
 use App\Service\ScenarioService;
 use ClickHouseDB\Client;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -25,46 +26,76 @@ final class CheckClickHouseCommand extends Command
 
     private Client $client;
 
+    private LoggerInterface $logger;
+
     public function __construct(
         NotifierInterface $notifier,
-        ScenarioService $scenarioService,
-        Client $client
+        ScenarioService   $scenarioService,
+        Client            $client,
+        LoggerInterface   $logger
     ) {
         parent::__construct();
         $this->notifier = $notifier;
         $this->scenarioService = $scenarioService;
         $this->client = $client;
+        $this->logger = $logger;
     }
 
     protected function configure(): void
     {
         $this->addArgument('scenario', InputArgument::REQUIRED, 'Scenario to execute (with .yml)');
+        $this->addOption(
+            'secondsBetweenQueries',
+            's',
+            InputArgument::OPTIONAL,
+            'Seconds between queries to sleep.',
+            1
+        );
+        $this->addOption(
+            'executeTimes',
+            't',
+            InputArgument::OPTIONAL,
+            'Run scenario times.',
+            100
+        );
         parent::configure();
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $output->writeln(self::COMMAND_PREFIX.' Command started');
+        $this->logger->debug(self::COMMAND_PREFIX.' Command started');
 
         $scenarioName = (string) $input->getArgument('scenario');
-        $output->writeln(self::COMMAND_PREFIX." Scenario name `$scenarioName`");
+        $this->logger->debug(self::COMMAND_PREFIX." Scenario name `$scenarioName`");
         $scenario = $this->scenarioService->getScenario($scenarioName);
+        $executedTimes = 0;
 
-        /** @var string $query */
-        foreach ($scenario as $check => $query) {
-            $output->writeln(self::COMMAND_PREFIX." Check `$check`. Query `$query`");
+        while (true) {
+            /** @var string $query */
+            foreach ($scenario as $check => $query) {
+                $this->logger->debug(self::COMMAND_PREFIX." Check `$check`. Query `$query`");
 
-            $result = $this->client->select($query)->rows();
-            $resultCount = count($result);
-            $output->writeln(self::COMMAND_PREFIX." Check `$check`. Result count $resultCount");
+                $result = $this->client->select($query)->rows();
+                $resultCount = count($result);
+                $this->logger->debug(self::COMMAND_PREFIX." Check `$check`. Result count $resultCount");
 
-            if ($resultCount > 0) {
-                $output->writeln(self::COMMAND_PREFIX." Check `$check` failed. Sending notification");
-                $this->notifier->send(new Notification("Check `$check` failed. Result: $resultCount"));
+                if ($resultCount > 0) {
+                    $this->logger->debug(self::COMMAND_PREFIX." Check `$check` failed. Sending notification");
+                    $this->notifier->send(
+                        new Notification(date('Y-m-d H:i:s')." Check `$check` failed. Result: $resultCount")
+                    );
+                }
+
+                sleep((int) $input->getOption('secondsBetweenQueries'));
+            }
+
+            $executedTimes++;
+            if ($executedTimes === (int) $input->getOption('executeTimes')) {
+                break;
             }
         }
 
-        $output->writeln(self::COMMAND_PREFIX.' Command finished');
+        $this->logger->debug(self::COMMAND_PREFIX.' Command finished');
 
         return self::SUCCESS;
     }
